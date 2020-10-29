2cfdd3ca6d00da22bd7c386788dfc066420962ae
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2020-10-29 14:52:58 -0600

    Add styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Header from './components/Header'
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <Header
-      titulo="Cotizador de seguros"
-    />
+    <Contenedor>
+      <Header
+        titulo="Cotizador de seguros"
+      />
+      <ContenedorFormulario>
+
+          
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
