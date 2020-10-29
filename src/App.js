11b0f966922a3706be64a83c0e92ed8f64f6caa4
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2020-10-29 16:36:47 -0600

    helpers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import Header from './components/Header'
+import React, {useState} from 'react';
 import styled from '@emotion/styled'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+import Resumen from './components/Resumen'
+import Resultado from './components/Resultado'
 
 const Contenedor = styled.div`
   max-width: 600px;
@@ -13,14 +16,34 @@ const ContenedorFormulario = styled.div`
 `
 
 function App() {
+
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: '',
+    }
+  })
+
+  //extraer datos 
+  const { cotizacion, datos } = resumen 
+
   return (
     <Contenedor>
       <Header
         titulo="Cotizador de seguros"
       />
-      <ContenedorFormulario>
-
-          
+      <ContenedorFormulario> 
+        <Formulario
+          guardarResumen={guardarResumen}
+        />
+        <Resumen 
+          datos={datos}
+        />
+        <Resultado
+          cotizacion={cotizacion}
+        />
       </ContenedorFormulario>
     </Contenedor>
   );
---
+++ src/components/Resumen.js
@@ -0,0 +1,32 @@
+import React, {Fragment} from 'react';
+import styled from '@emotion/styled'
+import {primerMayuscula} from './../helpers'
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #fff;
+    margin-top: 1rem;
+`
+
+const Resumen = ({datos}) => {
+
+    //extraer de datos
+    const {marca, year, plan} = datos
+
+    if(marca === '' || year === '' || plan === '') return null
+
+    return ( 
+        <ContenedorResumen>
+            <h2>Resumen de Cotizacion</h2>
+            <ul>
+                <li>Marca: {primerMayuscula(marca)}</li>
+                <li>Año del Auto: {year} </li>
+                <li>Plan: {primerMayuscula(plan)}</li>
+            </ul>
+        </ContenedorResumen>
+    );
+}
+
+export default Resumen;
